9530b0a6712b8ef332a221a21c358b0f0e8b1b56
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2024-03-16 18:03:55 +0400

    Add files via upload

---
+++ scraping_with_BeautifulSoup.py
@@ -0,0 +1,32 @@
+import requests
+from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+
+
+def store_in_file(soup, file):
+    for link in soup.find_all(class_="card-body"):
+        file.write('Title: ' + link.find(class_="title").get('title') + '\n')
+        file.write('Price: ' + link.find(class_='float-end price card-title pull-right').text + '\n')
+        file.write('Description: ' + link.find(class_='description card-text').text + '\n')
+        file.write('Reviews: ' + link.find(class_='float-end review-count').text + '\n\n')
+
+
+def scrap_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    else:
+        html_doc = response.text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        return soup
+
+
+if __name__ == '__main__':
+    url = 'https://webscraper.io/test-sites/e-commerce/allinone'
+    soup = scrap_data(url)
+    with open('scraped_data.txt', 'w') as file:
+        store_in_file(soup, file)
